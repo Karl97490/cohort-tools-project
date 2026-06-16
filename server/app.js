62c042359b766a0c019f438984886730af08e02f
okafordarlington98 <281412446+okafordarlington98@users.noreply.github.com>
2026-06-16 19:12:52 +0200

    Create model and schema for cohort collection

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 process.loadEnvFile()
 
+ 
 const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
@@ -8,16 +9,21 @@ const PORT = process.env.PORT || 5005
 const CLIENT_PORT = process.env.CLIENT_PORT || 5173
 
 const mongoose = require('mongoose')
+const Cohort = require('./models/Cohorts.model') // Model Cohort from the models folder and Cohorts.model.js
+
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const cohorts = require('./data/cohorts.json')
+const students = require('./data/students.json')
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express()
+
 mongoose.connect('mongodb://127.0.0.1:27017/cohort-tools-api')
   .then(x => console.log(`Connected to the database ${x.connections[0].name}`))
   .catch(error => console.log(`Connection error${error}`))
+  
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
@@ -39,20 +45,25 @@ app.get('/docs', (req, res) => {
   res.sendFile(__dirname + '/views/docs.html')
 })
 
+
+app.get('/api/cohorts', (req, res) => {
+  // res.json(cohorts)
+  Cohort.find({})
+  .then((cohorts) => {
+    // console.log("Retrieved books ->", books);
+    res.json(cohorts);
+  })
+  .catch((error) => {
+    // console.error("Error while retrieving books ->", error);
+    res.status(500).json({ error: "Failed to retrieve books" });
+  });
+})
+
+app.get('/api/students', (request, response) => {
+  response.json(students)
+})
+
 // START SERVER
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
-
-const cohorts = require('./data/cohorts.json')
-
-const students = require('./data/students.json')
-
-app.get('/api/cohorts', (req, res) => {
-  res.json(cohorts)
-})
-
-app.get('/api/students', (req, res) => {
-  res.json(students)
-})
-
