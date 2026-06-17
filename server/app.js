cede1fb64e76d1af23d176f8a4864758439eb575
yemi <o.ogunbadejo7@gmail.com>
2026-06-17 17:26:12 +0100

    Implemented CRUD Routes for Students and Cohorts

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 process.loadEnvFile()
 
- 
 const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
@@ -9,21 +8,22 @@ const PORT = process.env.PORT || 5005
 const CLIENT_PORT = process.env.CLIENT_PORT || 5173
 
 const mongoose = require('mongoose')
-const Cohort = require('./models/Cohorts.model') // Model Cohort from the models folder and Cohorts.model.js
-
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-const cohorts = require('./data/cohorts.json')
-const students = require('./data/students.json')
+const Cohort = require('./models/Cohorts.model') // Model Cohort from the models folder and Cohorts.model.js
+const Student = require('./models/Students.model')
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express()
 
-mongoose.connect('mongodb://127.0.0.1:27017/cohort-tools-api')
-  .then(x => console.log(`Connected to the database ${x.connections[0].name}`))
-  .catch(error => console.log(`Connection error${error}`))
-  
+mongoose
+  .connect('mongodb://127.0.0.1:27017/cohort-tools-api')
+  .then((x) =>
+    console.log(`Connected to the database ${x.connections[0].name}`),
+  )
+  .catch((error) => console.log(`Connection error${error}`))
+
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
@@ -45,22 +45,123 @@ app.get('/docs', (req, res) => {
   res.sendFile(__dirname + '/views/docs.html')
 })
 
-
+//Cohorts Routes
 app.get('/api/cohorts', (req, res) => {
   // res.json(cohorts)
   Cohort.find({})
-  .then((cohorts) => {
-    // console.log("Retrieved books ->", books);
-    res.json(cohorts);
-  })
-  .catch((error) => {
-    // console.error("Error while retrieving books ->", error);
-    res.status(500).json({ error: "Failed to retrieve books" });
-  });
+    .then((cohorts) => {
+      // console.log("Retrieved books ->", books);
+      res.json(cohorts)
+    })
+    .catch((error) => {
+      // console.error("Error while retrieving books ->", error);
+      res.status(500).json({ error: 'Failed to retrieve books' })
+    })
 })
 
-app.get('/api/students', (request, response) => {
-  response.json(students)
+app.get('/api/cohorts/:cohortId', async (req, res) => {
+  try {
+    const response = await Cohort.findById(req.params.cohortId)
+    res.json(response)
+  } catch (error) {
+    res.json(error)
+  }
+})
+
+app.post('/api/cohorts', async (req, res) => {
+  console.log(req.body)
+  try {
+    const response = await Cohort.create(req.body)
+    res.json(response)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.delete('/api/cohorts/:cohortId', async (req, res) => {
+  try {
+    const response = await Cohort.findByIdAndDelete(req.params.cohortId)
+    // res.json(response)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.put('/api/cohorts/:cohortId', async (req, res) => {
+  try {
+    const response = await Cohort.findByIdAndUpdate(
+      req.params.cohortId,
+      req.body,
+      { new: true },
+    )
+    res.json(response)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+// Students Routes
+app.get('/api/students', (req, res) => {
+  Student.find({})
+    .then((students) => {
+      res.json(students)
+    })
+    .catch((error) => {
+      console.log(error)
+      res.json(error)
+    })
+})
+
+app.get('/api/students/cohort/:cohortId', async (req, res) => {
+  try {
+    // Student.find({cohort: })
+    const response = await Student.find({ cohort: req.params.cohortId })
+    res.json(response)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.get('/api/students/:studentId', async (req, res) => {
+  try {
+    const response = await Student.findById(req.params.studentId)
+    res.json(response)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.post('/api/students', async (req, res) => {
+  try {
+    const response = await Student.create(req.body)
+    res.json(response)
+  } catch (error) {
+    console.log(error)
+    res.json(error)
+  }
+})
+
+app.put('/api/students/:studentId', async (req, res) => {
+  try {
+    const response = await Student.findByIdAndUpdate(
+      req.params.studentId,
+      req.body,
+      { new: true },
+    )
+    res.json(response)
+  } catch (error) {
+    console.log(error)
+    res.json(error)
+  }
+})
+
+app.delete('/api/students/:studentId', async (req, res) => {
+  try {
+    const response = await Student.findByIdAndDelete(req.params.studentId)
+    res.json(response)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 // START SERVER
--- server/models/Cohorts.model.js
+++ server/models/Cohorts.model.js
@@ -1,23 +1,35 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema // Schema class from mongoose package to create schema obj
 
-
 const cohortSchema = new Schema({
-    cohortSlug: {type: String, unique: true, required: true},
-    cohortName: {type:String, required: true},
-    program: {type:String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]},
-    format: {type:String, enum: ["Full Time", "Part Time"]},
-    campus: {type:String, enum: ["Madrid", "Barcelona", "Miami", "Paris", "Berlin", "Amsterdam", "Lisbon", "Remote"]},
-    startDate: {type:Date, default: Date.now()},
-    endDate: Date,
-    inProgress: {type:Boolean, default: false},
-    programManager: {type:String, required: true},
-    leadTeacher: {type:String, required: true},
-    totalHours: {type:Number, default: 360},
+  cohortSlug: { type: String, unique: true, required: true },
+  cohortName: { type: String, required: true },
+  program: {
+    type: String,
+    enum: ['Web Dev', 'UX/UI', 'Data Analytics', 'Cybersecurity'],
+  },
+  format: { type: String, enum: ['Full Time', 'Part Time'] },
+  campus: {
+    type: String,
+    enum: [
+      'Madrid',
+      'Barcelona',
+      'Miami',
+      'Paris',
+      'Berlin',
+      'Amsterdam',
+      'Lisbon',
+      'Remote',
+    ],
+  },
+  startDate: { type: Date, default: Date.now() },
+  endDate: Date,
+  inProgress: { type: Boolean, default: false },
+  programManager: { type: String, required: true },
+  leadTeacher: { type: String, required: true },
+  totalHours: { type: Number, default: 360 },
 })
 
-const Cohort = mongoose.model("Cohort", cohortSchema)
+const Cohort = mongoose.model('Cohort', cohortSchema)
 
 module.exports = Cohort
-
-
